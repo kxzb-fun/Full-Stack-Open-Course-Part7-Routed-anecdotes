8f4731aae48065e941c239d82e80f2b2f1a36390
kxzb-fun <sunday_ever@163.com>
2024-06-10 12:20:43 +0800

    feat: 把添加表单变成做自己定义的hook  finished 7.4-7.6

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import {
   Routes, Route, Link, useNavigate, useMatch
 } from 'react-router-dom'
+import  { useField } from './hooks'
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -55,22 +57,35 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  // const [content, setContent] = useState('')
+  // const [author, setAuthor] = useState('')
+  // const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const navigate = useNavigate()
+
+  const resetAll = (e)=>{
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
-    props.setNotification(content)
+    props.setNotification(content.value)
+    // 清空数据
+    resetAll()
   }
+  const omitReset = ({ reset, ...rest }) => rest
 
   return (
     <div>
@@ -78,17 +93,19 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...omitReset(content)} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...omitReset(author)} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          {/* <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} /> */}
+          <input name='info' {...omitReset(info)} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button onClick={resetAll}>reset</button>
       </form>
     </div>
   )
